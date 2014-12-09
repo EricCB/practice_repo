b053842774e7bfa7bc4fe71ca608a7172643e89c
Eric Clausen-Brown <eclausenbrown@recondynamics.com>
2014-12-09 15:28:13 -0800

    Second commit

--- practice.py
+++ practice.py
@@ -1,5 +1,5 @@
 import numpy as np
 import os
-from function_grabbag import filepath 
+from function_grabbag import filepath as fp
 
-print "I can put test.py right here: ", filepath.filepath('test.py')
+print "I can put test.py right here: ", fp.filepath('test.py')
